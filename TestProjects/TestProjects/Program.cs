353f91652a53ddf753bfdbb024739be437b31667
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-01-12 22:17:09 +0530

    adding fibonocciseries

---
+++ TestProjects/TestProjects/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME");
+            Console.ReadLine();
+            FibonacciSeries fibonacci = new FibonacciSeries();
+            fibonacci.FindFibonacciSeries();
+            Console.ReadLine();
+        }
+    }
+}
+ 
